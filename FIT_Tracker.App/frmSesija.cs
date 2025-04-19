71f51b3e7e839e0f88940871531da233eb385d39
tarikkisija14 <tarik.kisija@hotmail.com>
2025-04-19 22:10:50 +0200

    Izgled nove sesije

--- FIT_Tracker.App/frmSesija.cs
+++ FIT_Tracker.App/frmSesija.cs
@@ -9,6 +9,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,15 +18,21 @@ namespace FIT_Tracker.App
 {
     public partial class frmSesija : Form
     {
-        private GodinaStudija? godina;
-        private Semestar? semestar;
-        private Predmet? predmet;
-        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
-        private bool Aktivna;
-        private TimeSpan trajanje; 
-        private System.Windows.Forms.Timer timerclose = new System.Windows.Forms.Timer();
-        FITContext _context = new FITContext();
-        private DateTime startTime;
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        public  System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        public  System.Windows.Forms.Timer timerclose = new System.Windows.Forms.Timer();
+        public  FITContext _context = new FITContext();
+        public  GodinaStudija? godina;
+        public  Semestar? semestar;
+        public  Predmet? predmet;
+
+        public DateTime startTime;
+        public TimeSpan trajanje;
+        public bool Aktivna;
+
+
 
         public frmSesija(GodinaStudija? godina, Semestar? semestar, Predmet? predmet)
         {
@@ -33,56 +40,80 @@ namespace FIT_Tracker.App
             this.godina = godina;
             this.semestar = semestar;
             this.predmet = predmet;
+
+            InitForma();
+
+        }
+
+        private void InitForma()
+        {
+            // UI izgled
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+
+            lblPredmet.Font = new Font("Segoe UI", 20, FontStyle.Bold);
+            lblPredmet.ForeColor = Color.White;
+            lblPredmet.BackColor = Color.Transparent;
+
+            // Timer
+            trajanje = TimeSpan.Zero;
+            startTime = DateTime.Now.Date;
+            Aktivna = false;
+
             timer.Interval = 1000;
             timer.Tick += Timer_Tick;
-            trajanje = TimeSpan.Zero; 
-            Aktivna = false;
-            startTime = DateTime.Now.Date;
 
-
+            // Dugmići
+            Button[] dugmici = { btnZapocni, button2, button3 };
+            foreach (Button btn in dugmici)
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderColor = Color.White;
+                btn.FlatAppearance.BorderSize = 1;
+                btn.BackColor = Color.Transparent;
+                btn.ForeColor = Color.White;
+                btn.Cursor = Cursors.Hand;
+            }
         }
 
         private void Timer_Tick(object? sender, EventArgs e)
         {
-            trajanje = trajanje.Add(TimeSpan.FromSeconds(1)); 
+            trajanje = trajanje.Add(TimeSpan.FromSeconds(1));
             UpdateTimer();
         }
 
         private void UpdateTimer()
         {
-            if (trajanje.Hours > 0)
-            {
-                lblTimer.Text = string.Format("{0}h {1:D2}m {2:D2}s", trajanje.Hours, trajanje.Minutes, trajanje.Seconds);
-            }
-            else if (trajanje.Minutes > 0)
-            {
-                lblTimer.Text = string.Format("{0:D2}m {1:D2}s", trajanje.Minutes, trajanje.Seconds);
-            }
-            else
-            {
-                lblTimer.Text = string.Format("{0:D2}s", trajanje.Seconds);
-            }
+            string vrijeme =
+                trajanje.Hours > 0 ? $"{trajanje.Hours}h {trajanje.Minutes:D2}m {trajanje.Seconds:D2}s" :
+                trajanje.Minutes > 0 ? $"{trajanje.Minutes:D2}m {trajanje.Seconds:D2}s" :
+                $"{trajanje.Seconds:D2}s";
+
+            lblTimer.Text = vrijeme;
+            lblTimer.Font = new Font("Segoe UI", 28, FontStyle.Bold);
+            lblTimer.ForeColor = Aktivna ? Color.Red : Color.LightGray;
+            lblTimer.BackColor = Color.White;
+            lblTimer.TextAlign = ContentAlignment.MiddleCenter;
         }
 
         private void frmSesija_Load(object sender, EventArgs e)
         {
-            lblPredmet.Text = $"{predmet.Naziv}";
-            trajanje = TimeSpan.Zero; 
+            lblPredmet.Text = predmet?.Naziv;
             UpdateTimer();
-           
         }
 
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            using (LinearGradientBrush brush = new LinearGradientBrush(
-                this.ClientRectangle,
+            using var brush = new LinearGradientBrush(
+                ClientRectangle,
                 Color.FromArgb(30, 144, 255),
-                Color.FromArgb(0, 0, 139),  
-                LinearGradientMode.Vertical))
-            {
-                e.Graphics.FillRectangle(brush, this.ClientRectangle);
-            }
+                Color.FromArgb(0, 0, 139),
+                LinearGradientMode.Vertical);
+
+            e.Graphics.FillRectangle(brush, ClientRectangle);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
         }
 
 
@@ -107,12 +138,16 @@ namespace FIT_Tracker.App
 
         private void btnZapocni_MouseEnter(object sender, EventArgs e)
         {
-            btnZapocni.BackColor = Color.RoyalBlue;
+            var btn = (Button)sender;
+            btn.BackColor = Color.FromArgb(70, Color.White);
+            btn.ForeColor = Color.Black;
         }
 
         private void btnStart_MouseLeave(object sender, EventArgs e)
         {
-            btnZapocni.BackColor = Color.DodgerBlue;
+            var btn = (Button)sender;
+            btn.BackColor = Color.Transparent;
+            btn.ForeColor = Color.White;
         }
 
 
@@ -125,41 +160,36 @@ namespace FIT_Tracker.App
                 timer.Stop();
             }
 
-            frmNazivSesije nazivform=new frmNazivSesije();
+            var nazivForm = new frmNazivSesije();
 
-            if(nazivform.ShowDialog() == DialogResult.OK)
+            if (nazivForm.ShowDialog() == DialogResult.OK)
             {
-                SpremiSesiju(nazivform.Naziv);
+                SpremiSesiju(nazivForm.Naziv);
             }
             else
             {
-                trajanje = TimeSpan.Zero; 
+                trajanje = TimeSpan.Zero;
                 UpdateTimer();
-                Task.Delay(1000).ContinueWith(_ => this.Invoke((Action)(() => this.Close())));
+                Task.Delay(1000).ContinueWith(_ => Invoke((Action)(Close)));
                 return;
             }
 
-
             trajanje = TimeSpan.Zero;
             UpdateTimer();
-            Task.Delay(1000).ContinueWith(_ => this.Invoke((Action)(() => this.Close())));
+            Task.Delay(1000).ContinueWith(_ => Invoke((Action)(Close)));
         }
 
         private void SpremiSesiju(string naziv)
         {
-            string trajanjeFormatted;
-
-            if (trajanje.Hours > 0)
-                trajanjeFormatted = $"{trajanje.Hours}h {trajanje.Minutes:D2}m {trajanje.Seconds:D2}s";
-            else if (trajanje.Minutes > 0)
-                trajanjeFormatted = $"{trajanje.Minutes}m {trajanje.Seconds:D2}s";
-            else
-                trajanjeFormatted = $"{trajanje.Seconds}s";
+            string trajanjeFormatted =
+                 trajanje.Hours > 0 ? $"{trajanje.Hours}h {trajanje.Minutes:D2}m {trajanje.Seconds:D2}s" :
+                 trajanje.Minutes > 0 ? $"{trajanje.Minutes}m {trajanje.Seconds:D2}s" :
+                 $"{trajanje.Seconds}s";
 
             var nova = new Sesija
             {
                 Naziv = naziv,
-                PredmetId = predmet.Id,
+                PredmetId = predmet?.Id ?? 0,
                 Trajanje = trajanjeFormatted,
                 Start = startTime,
                 Finish = DateTime.Now
@@ -173,8 +203,6 @@ namespace FIT_Tracker.App
         private void timerclose_Tick(object? sender, EventArgs e)
         {
             timerclose.Stop();
-
-           
             this.Close();
         }
     }
